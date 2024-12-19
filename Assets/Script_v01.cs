eb15d011e4a0d84cec9645da6a7d47c89d0f2849
Shabanov <e.shabanov@carx-tech.com>
2024-12-19 14:39:53 +0300

    first script

---
+++ Assets/Script_v01.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 5;
+            int b = 6;
+            int c = 10;
+
+            byte bb;
+            sbyte sb;
+            short s;
+            ushort us;
+            int i;
+            uint ui;
+            long l;
+            ulong ul;
+            float f = 6.2f;
+            double d = 5.7;
+
+            char cc = '5';
+
+            string str = "Hello World";
+
+            bool bl;
+
+            Console.WriteLine(str);
+        }
+    }
+}
